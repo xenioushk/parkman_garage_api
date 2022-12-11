aee92ee0af9b28f240cf7d14b292c643757e990b
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2022-12-11 15:40:33 +0200

    - Added api "READ", "CREATE", "UPDATE", "DELETE" feature.

--- src/index.php
+++ src/index.php
@@ -2,33 +2,25 @@
 
 declare(strict_types=1);
 
-// spl_autoload_register(function ($class) {
-//   require __DIR__ . "/inc/$class.php";
-// });
-
 if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
   require_once dirname(__FILE__) . '/vendor/autoload.php';
 }
 
 require_once "inc/Config/ErrorHandler.php";
+
+set_error_handler("ErrorHandler::handleError");
 set_exception_handler("ErrorHandler::handleEexception");
 
 use Inc\Config\Database;
 use Inc\Models\ProductGatewayModel;
 use Inc\Controllers\ProductController;
 
-
-
 // return all the response in json format.
 header("Content-type: application/json; charset=UTF-8");
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-// echo "<pre>";
-// print_r($parts);
-// echo "</pre>";
-
-if ($parts[1] != "products" || $parts[1] != "garages") {
+if ($parts[1] != "products" && $parts[1] != "garages") {
   http_response_code(404);
   exit;
 }
@@ -37,15 +29,19 @@ $id = $parts[2] ?? null;
 
 $database = new Database("mysql", "product_db", "root", "secret");
 
-
+$routing = $parts[1];
 
 switch ($routing) {
 
   case "products":
+
+    // Send DB connection info.
     $gateway = new ProductGatewayModel($database);
 
+    // Create instance of controller class.
     $contoller = new ProductController($gateway);
 
+    // Process Request.
     $contoller->processRequest($_SERVER["REQUEST_METHOD"], $id);
     break;
 
